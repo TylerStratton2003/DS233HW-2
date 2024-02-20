63864ba7e1da436daaf7d3e4500cff0b4c8b51f2
meghanbuchanan <160171560+meghanbuchanan@users.noreply.github.com>
2024-02-20 11:22:47 -0800

    Update getWeather.py to read in hourly data

--- getWeather.py
+++ getWeather.py
@@ -1,6 +1,6 @@
-import os
 import sys
 import requests
+import csv
 
 
 """
@@ -11,20 +11,57 @@ This is Home work 2, you are allowed to work in pairs, you can only use chatGPT 
 
 # Additional comment for this program, any bugs or creative functions?
 
+# This program takes in three arguments: Column Name, Keyword to access specific data, and CSV filename.
+
 """
 
-
-response = requests.get('https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=9tXHRpu5YkfMwpZt8ZC2XF2y0ZARlHQX')
-data = response.json()
-print(data)
-api_key = ""
+api_key = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=9tXHRpu5YkfMwpZt8ZC2XF2y0ZARlHQX"
 
 if len(sys.argv)<3:
     print("This script is going to collect weather data and store in a csv file:")
-    print("python "+sys.argv[0]+" \"air quality\" airquality.csv")
+    print("python "+sys.argv[0]+" \"Column_Name\" windSpeed windData.csv")
     sys.exit(0)
 
-keyword = sys.argv[1]
-addrOut = sys.argv[2]
+colName = sys.argv[1]
+keyword = sys.argv[2]
+addrOut = sys.argv[3]
 
-# put your code here 
+# put your code here
+response = requests.get(api_key)
+data = response.json()
+
+# dictionary with only hourly data
+hourly_data = data["timelines"]["hourly"]
+
+# new dictionary to hold extracted information about Wind
+windData = {"Date": [], "Time": [], colName: []}
+
+# iterates through each hour in the API
+for i in range(len(hourly_data)):
+    # timeline retrieves line to get info on Date and Time
+    timeline = hourly_data[i]["time"]
+
+    # this grabs the specific date and time for the i entry and adds to windSpeed
+    date = timeline[0:10]
+    time = timeline[12:19]
+    windData["Date"].append(date)
+    windData["Time"].append(time)
+
+    # this adds the wind direction for the given entry into the dictionary
+    windData[colName].append(hourly_data[i]["values"][keyword])
+
+
+csv_file = addrOut
+
+with open(csv_file, 'w', newline='') as file:
+    fieldnames = windData.keys()
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    # write the header
+    writer.writeheader()
+
+    # write the data
+    for row in zip(*windData.values()):
+        writer.writerow(dict(zip(fieldnames, row)))
+
+print("csv file was successfully created")
